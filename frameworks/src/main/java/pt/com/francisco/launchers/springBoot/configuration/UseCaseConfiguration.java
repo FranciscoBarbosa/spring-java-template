a206dd8ca46ef17407f5301dac5de7f670939f17
francisco.barbosa <franciscolamares@gmail.com>
2023-12-14 14:56:47 +0000

    chore: fix configuration

--- frameworks/src/main/java/pt/com/francisco/launchers/springBoot/configuration/UseCaseConfiguration.java
+++ frameworks/src/main/java/pt/com/francisco/launchers/springBoot/configuration/UseCaseConfiguration.java
@@ -2,14 +2,22 @@ package pt.com.francisco.launchers.springBoot.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import pt.com.francisco.launchers.springBoot.dataAccess.TaskGatewayImpl;
 import pt.com.francisco.useCases.databaseGateways.TaskGateway;
+import pt.com.francisco.useCases.inputPort.TaskUseCase;
 import pt.com.francisco.useCases.interactorPort.TaskUseCaseImpl;
 
 @Configuration
 public class UseCaseConfiguration {
 
     @Bean
-    TaskUseCaseImpl taskGateway(TaskGateway taskGateway){
+    TaskGateway taskGateway(){
+        return new TaskGatewayImpl();
+    }
+
+    @Bean
+    TaskUseCase taskUseCase(TaskGateway taskGateway){
         return new TaskUseCaseImpl(taskGateway);
     }
+
 }
